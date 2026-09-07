4b0428534265cc0e6096f497984ac6cac896e9eb
conejomultigestor <koky.je@gmail.com>
2026-09-06 20:56:28 -0400

    Takuro v2

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "takuro-v2";
+const FILES = ["./", "./index.html", "./css/estilo.css", "./js/config.js", "./js/crypto.js", "./js/live.js", "./js/app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open("takuro-runtime").then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((hit) => hit || caches.match("./index.html")))
+  );
+});
